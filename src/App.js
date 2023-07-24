bec3d66ecba8380e3eb99c922145afef94965174
Krishan Kant Sen <skrishan111213@gmail.com>
2023-07-24 19:00:05 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import react,{ useState } from 'react';
+import './App.css';
+const time=new Date().toLocaleTimeString();
+const date=new Date().toLocaleDateString();
+
+
+const App=()=>{
+const cime=useState();
+const[ctime,utime]=useState(time)
+const[cdate,udate]=useState(date);
+  const  UpdateTime=()=>{
+    const time=new Date().toLocaleTimeString();
+    utime(time);
+  }
+  const  UpdateDate=()=>{
+    const date=new Date().toLocaleDateString();
+    udate(date);
+  }
+  setInterval(UpdateTime,1000);
+  setInterval(UpdateDate,1000);
+  return(
+    <>
+    
+      <h1>Time : {ctime}</h1>
+      <h2>Date : {cdate}</h2>
+    </>
+  )
 }
 
 export default App;
