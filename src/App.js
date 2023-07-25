e00e159e670fcc0f325b53f0137f89940f89c837
Krishan Kant Sen <skrishan111213@gmail.com>
2023-07-25 17:47:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-
-import react,{ useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-const time=new Date().toLocaleTimeString();
-const date=new Date().toLocaleDateString();
 
+const time = new Date().toLocaleTimeString();
+const date = new Date().toLocaleDateString();
 
-const App=()=>{
-const cime=useState();
-const[ctime,utime]=useState(time)
-const[cdate,udate]=useState(date);
-  const  UpdateTime=()=>{
-    const time=new Date().toLocaleTimeString();
+const App = () => {
+  
+  const [ctime, utime] = useState(time);
+  const [cdate, udate] = useState(date);
+
+  const UpdateTime = () => {
+    const time = new Date().toLocaleTimeString();
     utime(time);
-  }
-  const  UpdateDate=()=>{
-    const date=new Date().toLocaleDateString();
+  };
+
+  const UpdateDate = () => {
+    const date = new Date().toLocaleDateString();
     udate(date);
-  }
-  setInterval(UpdateTime,1000);
-  setInterval(UpdateDate,1000);
-  return(
+  };
+
+  
+
+  setInterval(UpdateTime, 1000);
+  setInterval(UpdateDate, 1000);
+
+  return (
     <>
-    
-      <h1>Time : {ctime}</h1>
-      <h2>Date : {cdate}</h2>
+
+       <h1 className='myhead'>Digital Clock</h1>
+        <h1 className='myh'>Time: {ctime}</h1>
+        <h2>Date: {cdate}</h2>
+      
     </>
-  )
-}
+  );
+};
 
 export default App;
